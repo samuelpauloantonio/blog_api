1e417133e7ce9a38a491fcc30380be55e47526ee
Samuel Paulo António <samueldev1997@gmail.com>
2022-01-19 21:29:01 +0100

    feat: add swagger and configuration docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
+import { RequestMethod } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const version = process.env.npm_package_version;
+
+  app.setGlobalPrefix('api/v1', {
+    exclude: [{ path: '/', method: RequestMethod.GET }],
+  });
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Saúde iD - Blog API')
+    .setDescription('Saúde iD - Blog API with Node Js')
+    .setVersion(version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, configSwagger);
+
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
+
+  const port = process.env.PORT;
+  await app.listen(port);
+
+  console.log(`server running in port ${port}`);
 }
+
 bootstrap();
