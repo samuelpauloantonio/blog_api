c4e699276885ee92e05a743cd8a65a86ba1029d7
Samuel Paulo António <samueldev1997@gmail.com>
2022-01-20 12:17:48 +0100

    chore: config main server

---
+++ src/shared/infra/http/main.ts
@@ -0,0 +1,37 @@
+import 'dotenv/config';
+import { RequestMethod } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import configuration from 'src/config/configuration';
+import { AppModule } from 'src/modules/app.module';
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    const version = process.env.npm_package_version;
+
+    app.setGlobalPrefix('api/v1', {
+        exclude: [{ path: '/', method: RequestMethod.GET }],
+    });
+
+    const configSwagger = new DocumentBuilder()
+        .setTitle('Saúde iD - Blog API')
+        .setDescription('Saúde iD - Blog API with Node Js')
+        .setVersion(version)
+        .build();
+
+    const document = SwaggerModule.createDocument(app, configSwagger);
+
+    SwaggerModule.setup('api', app, document, {
+        swaggerOptions: {
+            tagsSorter: 'alpha',
+            operationsSorter: 'alpha',
+        },
+    });
+
+    const port = configuration.port;
+    await app.listen(port);
+
+    console.log(`server running in port ${port}`);
+}
+
+bootstrap();
