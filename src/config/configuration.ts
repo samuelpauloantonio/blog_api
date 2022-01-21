9226f5d2a83ecc4a366abc2b624f29e10e9445e1
Samuel Paulo António <samueldev1997@gmail.com>
2022-01-21 22:47:37 +0100

    chore: update env

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,8 +1,8 @@
 export default {
-    port: parseInt(process.env.PORT, 10) || 3000,
+    port: parseInt(process.env.PORT) || 3000,
     database: {
         host: process.env.MONGO_DATABASE_HOST,
-        port: parseInt(process.env.MONGO_DATABASE_PORT, 10) || 27017,
+        port: parseInt(process.env.MONGO_DATABASE_PORT) || 27017,
         database_name: process.env.MONGO_DATABASE_NAME,
         properties: {
             useNewUrlParser: true,
